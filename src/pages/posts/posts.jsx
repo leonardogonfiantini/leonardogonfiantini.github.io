0c7810a62e0367bcb341ba4aef2e732e5690b513
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-04-12 17:34:02 +0200

    navbar and bio responsive

--- src/pages/posts/posts.jsx
+++ src/pages/posts/posts.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Footer from '../../components/footer/footer'
+import Navbar from '../../components/navbar/navbar'
 import "./posts_style.scss"
 
 const Posts = () => {
     return (
-        <div> Posts </div>
+        <div> Posts 
+            <Navbar />
+        
+            <Footer />
+        </div>
     )
 }
 
