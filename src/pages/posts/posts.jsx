f3f18e86857f80c7c450fc8d077cdeaf9b0b7400
leonardogonfiantini <lp.gonfiantini@gmail.com>
2022-04-13 13:51:05 +0200

    Aggiunti works e posts, manca il contenuto.

--- src/pages/posts/posts.jsx
+++ src/pages/posts/posts.jsx
@@ -5,9 +5,22 @@ import "./posts_style.scss"
 
 const Posts = () => {
     return (
-        <div> Posts 
+        <div className='post'> 
             <Navbar />
-        
+                <div className='list-posts'>
+                    <ul>
+                        <li>  Link1  </li>
+                        <li>  Link2  </li>
+                        <li>  Link3  </li>
+                        <li>  Link4  </li>
+                        <li>  Link5  </li>
+                        <li>  Link6  </li>
+                        <li>  Link7  </li>
+                        <li>  Link8  </li>
+                        <li>  Link9  </li>
+                        <li>  Link0  </li>
+                    </ul>
+                </div>
             <Footer />
         </div>
     )
--- src/pages/works/works.jsx
+++ src/pages/works/works.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./works_style.scss"
 
+import Footer from '../../components/footer/footer'
+import Navbar from '../../components/navbar/navbar'
+import Work from '../../components/work/work'
+
+import Ankuoo from '../../components/work/ankuoo.png'
+
 const Works = () => {
     return (
-        <div> Works </div>
+        <div className='works'> 
+            <Navbar />
+
+                <Work title="Ankuoo web app" img={Ankuoo} description="Ho ricreato l'app di ankuoo, per i dispositivi iot, si possono inserire timer, time schedule con orari e giorni ma anche accendere direttamente il dispositivo, l'applicazione utilizza mongodb atlas per tenere traccia delle operazioni"/>
+
+                <Work title="Ankuoo web app" img={Ankuoo} description="Ho ricreato l'app di ankuoo, per i dispositivi iot, si possono inserire timer, time schedule con orari e giorni ma anche accendere direttamente il dispositivo, l'applicazione utilizza mongodb atlas per tenere traccia delle operazioni"/>
+
+            <Footer />
+        </div>
     )
 }
 
